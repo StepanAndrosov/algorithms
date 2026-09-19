90e9e4f0a1ad0a952521a1efbc8e30e8bad97228
s37680930 <s37680930@gmail.com>
2026-09-19 17:23:53 +0700

    Place flowers

--- 1_level/arrays/place_flowers.ts
+++ 1_level/arrays/place_flowers.ts
@@ -5,7 +5,6 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
   for (let i = 0; i <= copybed.length - 1; i++) {
     const left = i === 0 ? 0 : copybed[i - 1];
     const right = i === copybed.length - 1 ? 0 : copybed[i + 1];
-
     if (copybed[i] === 0 && left === 0 && right === 0) {
       copybed[i] = 1;
       countFlowers--;
@@ -16,5 +15,5 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
   return false;
 }
 
-// console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
 console.log(canPlaceFlowers([1, 0, 0, 0, 0, 1], 2)); // false
